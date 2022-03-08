f55ec031a81f5efa84cf8e8af58e641e5661966e
connordunham <37425411+connordunham@users.noreply.github.com>
2022-03-08 15:58:05 -0500

    Add files via upload

--- AD5933.h
+++ AD5933.h
@@ -1,155 +1,171 @@
-#ifndef AD5933_h
-#define AD5933_h
+/***************************************************************************//**
+ *   @file   AD5933.h
+ *   @brief  Header file of AD5933 Driver.
+ *   @author DBogdan (dragos.bogdan@analog.com)
+********************************************************************************
+ * Copyright 2012(c) Analog Devices, Inc.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *  - Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *  - Neither the name of Analog Devices, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *  - The use of this software may or may not infringe the patent rights
+ *    of one or more patent holders.  This license does not release you
+ *    from the requirement that you obtain separate licenses from these
+ *    patent holders to use this software.
+ *  - Use of the software either in source or binary form, must be run
+ *    on or directly connected to an Analog Devices Inc. component.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT,
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, INTELLECTUAL PROPERTY RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+********************************************************************************
+ *   SVN Revision: $WCREV$
+*******************************************************************************/
+#ifndef __AD5933_H__
+#define __AD5933_H__
 
-/**
- * Includes
- */
-//#include <Arduino.h>
-//#include <wire.h>
-//#include <windows.h>
-#include <unistd.h>				//Needed for I2C port
-#include <fcntl.h>				//Needed for I2C port
-#include <sys/ioctl.h>			//Needed for I2C port
-#include <linux/i2c-dev.h>		//Needed for I2C port
-#include <stdio.h>
-//#include "libraries/Wire/Wire.h"
-//#include "libraries/ArduinoOTA/"
-/**
- * AD5933 Register Map
- *  Datasheet p23
- */
-// Device address and address pointer
-#define AD5933_ADDR     (0x0D)
-#define ADDR_PTR        (0xB0)
-// Control Register
-#define CTRL_REG1       (0x80)
-#define CTRL_REG2       (0x81)
-// Start Frequency Register
-#define START_FREQ_1    (0x82)
-#define START_FREQ_2    (0x83)
-#define START_FREQ_3    (0x84)
-// Frequency increment register
-#define INC_FREQ_1      (0x85)
-#define INC_FREQ_2      (0x86)
-#define INC_FREQ_3      (0x87)
-// Number of increments register
-#define NUM_INC_1       (0x88)
-#define NUM_INC_2       (0x89)
-// Number of settling time cycles register
-#define NUM_SCYCLES_1   (0x8A)
-#define NUM_SCYCLES_2   (0x8B)
-// Status register
-#define STATUS_REG      (0x8F)
-// Temperature data register
-#define TEMP_DATA_1     (0x92)
-#define TEMP_DATA_2     (0x93)
-// Real data register
-#define REAL_DATA_1     (0x94)
-#define REAL_DATA_2     (0x95)
-// Imaginary data register
-#define IMAG_DATA_1     (0x96)
-#define IMAG_DATA_2     (0x97)
+/******************************************************************************/
+/***************************** Include Files **********************************/
+/******************************************************************************/
+//#include "Communication.h"
 
-/**
- * Constants
- *  Constants for use with the AD5933 library class.
- */
-// Temperature measuring
-#define TEMP_MEASURE    (CTRL_TEMP_MEASURE)
-#define TEMP_NO_MEASURE (CTRL_NO_OPERATION)
-// Clock sources
-#define CLOCK_INTERNAL  (CTRL_CLOCK_INTERNAL)
-#define CLOCK_EXTERNAL  (CTRL_CLOCK_EXTERNAL)
-// PGA gain options
-#define PGA_GAIN_X1     (CTRL_PGA_GAIN_X1)
-#define PGA_GAIN_X5     (CTRL_PGA_GAIN_X5)
-// Power modes
-#define POWER_STANDBY   (CTRL_STANDBY_MODE)
-#define POWER_DOWN      (CTRL_POWER_DOWN_MODE)
-#define POWER_ON        (CTRL_NO_OPERATION)
-// I2C result success/fail
-#define I2C_RESULT_SUCCESS       (0)
-#define I2C_RESULT_DATA_TOO_LONG (1)
-#define I2C_RESULT_ADDR_NAK      (2)
-#define I2C_RESULT_DATA_NAK      (3)
-#define I2C_RESULT_OTHER_FAIL    (4)
-// Control register options
-#define CTRL_NO_OPERATION       (0b00000000)
-#define CTRL_INIT_START_FREQ    (0b00010000)
-#define CTRL_START_FREQ_SWEEP   (0b00100000)
-#define CTRL_INCREMENT_FREQ     (0b00110000)
-#define CTRL_REPEAT_FREQ        (0b01000000)
-#define CTRL_TEMP_MEASURE       (0b10010000)
-#define CTRL_POWER_DOWN_MODE    (0b10100000)
-#define CTRL_STANDBY_MODE       (0b10110000)
-#define CTRL_RESET              (0b00010000)
-#define CTRL_CLOCK_EXTERNAL     (0b00001000)
-#define CTRL_CLOCK_INTERNAL     (0b00000000)
-#define CTRL_PGA_GAIN_X1        (0b00000001)
-#define CTRL_PGA_GAIN_X5        (0b00000000)
-// Status register options
-#define STATUS_TEMP_VALID       (0x01)
-#define STATUS_DATA_VALID       (0x02)
-#define STATUS_SWEEP_DONE       (0x04)
-#define STATUS_ERROR            (0xFF)
-// Frequency sweep parameters
-#define SWEEP_DELAY             (1)
+extern int i2cdevice;
+extern float AD5933_CALIBRATION_IMPEDANCE;
 
-/**
- * AD5933 Library class
- *  Contains mainly functions for interfacing with the AD5933.
- */
-class AD5933 {
-    public:
-        // Reset the board
-        static bool reset(void);
+/******************************************************************************/
+/************************** AD5933 Definitions ********************************/
+/******************************************************************************/
 
-        // Temperature measuring
-        static bool enableTemperature(byte);
-        static double getTemperature(void);
+/* AD5933 Registers */
+#define AD5933_REG_CONTROL_HB       0x80    // HB of the Control register
+#define AD5933_REG_CONTROL_LB       0x81    // LB of the Control register
+#define AD5933_REG_FREQ_START       0x82    // Start frequency
+#define AD5933_REG_FREQ_INC         0x85    // Frequency increment
+#define AD5933_REG_INC_NUM          0x88    // Number of increments
+#define AD5933_REG_SETTLING_CYCLES  0x8A    // Number of settling time cycles
+#define AD5933_REG_STATUS           0x8F    // Status
+#define AD5933_REG_TEMP_DATA        0x92    // Temperature data
+#define AD5933_REG_REAL_DATA        0x94    // Real data
+#define AD5933_REG_IMAG_DATA        0x96    // Imaginary data
 
-        // Clock
-        static bool setClockSource(byte);
-        static bool setInternalClock(bool);
-        //bool setSettlingCycles(int); // not implemented - not used yet
+/* AD5933_REG_CONTROL_HB Bits */
+#define AD5933_CONTROL_FUNCTION(x)  ((x) << 4)     
+#define AD5933_CONTROL_RANGE(x)     ((x) << 1)
+#define AD5933_CONTROL_PGA_GAIN(x)  ((x) << 0)
 
-        // Frequency sweep configuration
-        static bool setStartFrequency(unsigned long);
-        static bool setIncrementFrequency(unsigned long);
-        static bool setNumberIncrements(unsigned int);
+/* AD5933_REG_CONTROL_LB Bits */
+#define AD5933_CONTROL_RESET        (0x1 << 4)
+#define AD5933_CONTROL_INT_SYSCLK   (0x0 << 3)
+#define AD5933_CONTROL_EXT_SYSCLK   (0x1 << 3)
 
-        // Gain configuration
-        static bool setPGAGain(byte);
+/* AD5933_CONTROL_FUNCTION(x) options */
+#define AD5933_FUNCTION_NOP                 0x0
+#define AD5933_FUNCTION_INIT_START_FREQ     0x1
+#define AD5933_FUNCTION_START_SWEEP         0x2
+#define AD5933_FUNCTION_INC_FREQ            0x3
+#define AD5933_FUNCTION_REPEAT_FREQ         0x4
+#define AD5933_FUNCTION_MEASURE_TEMP        0x9
+#define AD5933_FUNCTION_POWER_DOWN          0xA
+#define AD5933_FUNCTION_STANDBY             0xB
 
-        // Excitation range configuration
-        //bool setRange(byte, int); // not implemented - not used yet
+/* AD5933_CONTROL_RANGE(x) options */
+#define AD5933_RANGE_2000mVpp       0x0
+#define AD5933_RANGE_200mVpp        0x1
+#define AD5933_RANGE_400mVpp        0x2
+#define AD5933_RANGE_1000mVpp       0x3
 
-        // Read registers
-        static byte readRegister(byte);
-        static byte readStatusRegister(void);
-        static int readControlRegister(void);
+/* AD5933_CONTROL_PGA_GAIN(x) options */
+#define AD5933_GAIN_X5              0
+#define AD5933_GAIN_X1              1
 
-        // Impedance data
-        static bool getComplexData(int*, int*);
+/* AD5933_REG_STATUS Bits */
+#define AD5933_STAT_TEMP_VALID      (0x1 << 0)
+#define AD5933_STAT_DATA_VALID      (0x1 << 1)
+#define AD5933_STAT_SWEEP_DONE      (0x1 << 2)
 
-        // Set control mode register (CTRL_REG1)
-        static bool setControlMode(byte);
+/* AD5933 Address */
+#define AD5933_ADDRESS              0x0D
 
-        // Power mode
-        static bool setPowerMode(byte);
+/* AD5933 Block Commands */
+#define AD5933_BLOCK_WRITE          0xA0
+#define AD5933_BLOCK_READ           0xA1
+#define AD5933_ADDR_POINTER         0xB0
 
-        // Perform frequency sweeps
-        static bool frequencySweep(int real[], int imag[], int);
-        static bool calibrate(double gain[], int phase[], int ref, int n);
-        static bool calibrate(double gain[], int phase[], int real[],
-                              int imag[], int ref, int n);
-    private:
-        // Private data
-        static const unsigned long clockSpeed = 16776000;
+/* AD5933 Specifications */
+#define AD5933_INTERNAL_SYS_CLK     16000000ul      // 16MHz
+#define AD5933_MAX_INC_NUM          511             // Maximum increment number
+#define AD5933_CALIBRATION_RFB		20000			// Calibration voltage-to-current gain feedback resistor is 20k for the pmodIA board
 
-        // Sending/Receiving byte method, for easy re-use
-        static int getByte(byte, byte*);
-        static bool sendByte(byte, byte);
-};
 
-#endif
+
+/******************************************************************************/
+/************************ Functions Declarations ******************************/
+/******************************************************************************/
+
+/*! Initializes the communication peripheral. */
+char AD5933_Init(void);
+
+/*! Writes data into a register. */
+void AD5933_SetRegisterValue(unsigned char registerAddress,
+                             unsigned long registerValue,
+                             unsigned char bytesNumber);
+
+/*! Reads the value of a register. */
+unsigned long AD5933_GetRegisterValue(unsigned char registerAddress,
+                                      unsigned char bytesNumber);
+
+/*! Resets the device. */
+void AD5933_Reset(void);
+
+/*! Selects the source of the system clock. */
+void AD5933_SetSystemClk(char clkSource, unsigned long extClkFreq);
+
+/*! Selects the range and gain of the device. */
+void AD5933_SetRangeAndGain(char range, char gain);
+
+/*! Reads the temp. from the part and returns the data in degrees Celsius. */
+float AD5933_GetTemperature(void);
+
+/*! Configures the sweep parameters. */
+void AD5933_ConfigSweep(unsigned long  startFreq,
+                        unsigned long  incFreq,
+                        unsigned short incNum);
+
+/*! Starts the sweep operation. */
+void AD5933_StartSweep(void);
+
+double AD5933_CalculateGainFactor(unsigned long calibrationImpedance,
+							   char freqFunction);
+double AD5933_CalculateImpedance(double gainFactor,
+								 char freqFunction);
+
+
+
+
+/*! Reads the real and the imaginary data and calculates the Gain Factor. */
+/*double AD5933_CalculateGainFactor(unsigned long calibrationImpedance,
+                                  unsigned char freqFunction);*/
+
+/*! Reads the real and the imaginary data and calculates the Impedance. */
+/*double AD5933_CalculateImpedance(double gainFactor,
+                                 unsigned char freqFunction);*/
+
+#endif /* __AD5933_H__ */
